3990c6386dfc9d590bfac9a155f6579b1492e717
itsStxn <heboivan@gmail.com>
2023-04-21 11:05:54 +0200

    Handling css

---
+++ assets/script/script.js
@@ -0,0 +1,65 @@
+var cont2 = document.querySelector(".js-cont2");
+var cont3 = document.querySelector(".js-cont3");
+var cont4 = document.querySelector(".js-cont4");
+
+window.onscroll = function(){
+    var opacity_c2 = resetopacity(getPercentageVisible(cont2) / 100);
+    var opacity_c3 = resetopacity(getPercentageVisible(cont3) / 100);
+    var opacity_c4 = resetopacity(getPercentageVisible(cont4) / 100);
+
+    var xmove_c2 = resetmove(getPercentageVisible(cont2), "right");
+    var xmove_c3 = resetmove(getPercentageVisible(cont3)*-1, "left");
+    var xmove_c4 = resetmove(getPercentageVisible(cont4), "right");
+
+    cont2.style.opacity = `${opacity_c2}`;
+    cont3.style.opacity = `${opacity_c3}`;
+    cont4.style.opacity = `${opacity_c4}`;
+
+    trigger_move(cont2, xmove_c2);
+    trigger_move(cont3, xmove_c3);
+    trigger_move(cont4, xmove_c4);
+}
+
+
+function getPercentageVisible(el) {
+    const rect = el.getBoundingClientRect();
+    const elementTop = rect.top;
+    const elementHeight = rect.height;
+    const visibleHeight = Math.min(elementHeight, window.innerHeight - elementTop);
+    return (visibleHeight / elementHeight) * 100;
+}
+function resetmove($value, $direction){
+    if ($direction == "right"){
+        if ($value < -100){
+            $value = -100;
+        }
+        if ($value > 0){
+            $value = 0;
+        }
+    }
+    else {
+        if ($value < 0){
+            $value = 0;
+        }
+        if ($value > 100){
+            $value = 100;
+        }
+    }
+    return $value;
+}
+function resetopacity($value){
+    if ($value < 0){
+        $value = 0;
+    }
+    if ($value > 1){
+        $value = 1;
+    }
+    return $value;
+}
+function trigger_move($el, $xmove){
+    if (getPercentageVisible($el) <= 10){
+        $el.style.transform = `translateX(-100%)`;
+    } else {
+        $el.style.transform = `translateX(${$xmove}%)`;
+    }
+}
--- index.php
+++ index.php
@@ -11,15 +11,54 @@
     <div class="css-back1"></div>
     <div class="col page">
         <div class="css-cont1 row">
-            <div class="css-back2">
-                <h2>Time to start saving peacefully</h2>
-                <p>In a user friendly enviroment</p>
+            <span class="css-head1">
+                <img src="./assets/pics/pic6.png" alt="Eden Bank logo">
+            </span>
+            <span class="css-head2 row">
+                <div class="css-back2">
+                    <h2>Time to start saving <strong>peacefully</strong></h2>
+                    <p>In a user friendly enviroment</p>
+                </div>
+                <div class="css-pagehead">
+                    <span class="col">
+                        <h2>Welcome to the Eden</h2>
+                        <p>Manage your savings with us!</p>
+                    </span>
+                    <a href="https://github.com/Stxn02/Eden-bank-website" target="_blank">Visit our GitHub</a>
+                </div>
+            </span>
+        </div>
+        <div class="css-cont2 js-cont2 row">
+            <div class="css-details col">
+                <span class="row">
+                    <div id="evelien"></div>
+                    <div class="css-text">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius eaque vel cumque ullam minus, perspiciatis, ab et itaque magni fugit expedita natus dolorem laboriosam omnis. Quasi quisquam aspernatur eligendi? Ab.</p>
+                    </div>
+                </span>
+                <span class="row right">
+                    <div id="stan"></div>
+                    <div class="css-text">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius eaque vel cumque ullam minus, perspiciatis, ab et itaque magni fugit expedita natus dolorem laboriosam omnis. Quasi quisquam aspernatur eligendi? Ab.</p>
+                    </div>
+                </span>
             </div>
-            <div class="css-pagehead">
-                <h2>Welcome to the Eden</h2>
-                <p>Manage your savings with us!</p>
-                <a href="https://github.com/Stxn02/Eden-bank-website" target="_blank">Visit our GitHub</a>
+            <h2>Collaborators</h2>
+            <img class="css-deco" src="./assets/pics/pic8.png" alt="Collaborators pic">
+        </div>
+        <div class="css-cont3 js-cont3 row_rev">
+            <div class="css-details">
+                <img src="./assets/pics/pic7.png" alt="Wireframe">
             </div>
+            <h2>Wireframe</h2>
+            <img class="css-deco" src="./assets/pics/pic10.png" alt="Wireframe pic">
+        </div>
+        <div class="css-cont4 js-cont4 row">
+            <div class="css-details">
+                <img src="./assets/pics/pic12.png" alt="Erd diagram">
+            </div>
+            <h2>ERD diagram</h2>
+            <img class="css-deco" src="./assets/pics/pic11.png" alt="ERD diagram pic">
         </div>
     </div>
     <script src="./assets/script/script.js"></script>
